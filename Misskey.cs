6e0f42cd457c81a3b133c0b21c45554e092ecd95
pen <pen@penginn.net>
2024-01-04 01:24:29 +0900

    プロジェクト ファイルを追加します。

---
+++ Misskey.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MisskeyRemoteDeliver
+{
+
+    #region users/notes
+
+    public class NotesRequest_Rootobject
+    {
+        public string userId { get; set; }
+        public int limit { get; set; }
+    }
+    public class NotesRequest2_Rootobject
+    {
+        public string userId { get; set; }
+        public int limit { get; set; }
+        public string untilId { get; set; }
+    }
+
+    #endregion
+
+    #region ap/show
+    public class APShowRequest_Rootobject
+    {
+        public string i { get; set; }
+        public string uri { get; set; }
+    }
+    #endregion
+    internal class Misskey
+    {
+        static HttpClient Client = new HttpClient();
+
+        /// <summary>
+        /// APIトークン
+        /// </summary>
+        string AccessToken;
+
+        /// <summary>
+        /// 投稿先ホスト名
+        /// </summary>
+        string MissKeyHost;
+
+        /// <summary>
+        /// 設定の設定(?)
+        /// </summary>
+        /// <param name="Token"></param>
+        /// <param name="host"></param>
+        public void Setting(string Token, string host)
+        {
+            AccessToken = Token;
+            MissKeyHost = host;
+        }
+
+        /// <summary>
+        /// Post送信(Json)
+        /// </summary>
+        /// <param name="API">エンドポイント</param>
+        /// <param name="Json"></param>
+        /// <returns></returns>
+        public string Post(string API, string Json)
+        {
+            //   HttpResponseMessage Respone;
+            string ResultBody;
+            HttpStatusCode ResStatus = HttpStatusCode.NotFound;
+            var Content = new StringContent(Json, Encoding.UTF8, @"application/json");
+            try
+            {
+                var Respone = Client.PostAsync($"https://{MissKeyHost}/api/{API}", Content);
+
+                ResultBody = Respone.Result.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(ResultBody+"\r\n");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+
+            return ResultBody;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Text.Json;
+{
+   
+        var cfg = LoadConfig();
+        var Remote = new MisskeyRemoteDeliver.Misskey();
+        var Local  = new MisskeyRemoteDeliver.Misskey();
+        Remote.Setting(cfg.Remote_Token, cfg.Remote_Host);
+        Local.Setting(cfg.Local_Token, cfg.Local_Host);
+
+        foreach (var id in cfg.UserIDs)
+        {
+        var i = 0;
+        var Request_R1 = new MisskeyRemoteDeliver.NotesRequest_Rootobject();
+            Request_R1.limit = 100;
+            Request_R1.userId = id;
+            var Respone = Remote.Post("users/notes",System.Text.Json.JsonSerializer.Serialize(Request_R1));
+        try
+        {
+            var Result = JsonConvert.DeserializeObject<dynamic>(Respone);
+            if (Result is null)
+            { continue; }
+            string untilID = "";
+            var Request_L = new MisskeyRemoteDeliver.APShowRequest_Rootobject();
+            foreach (var obj in (Result as Newtonsoft.Json.Linq.JArray))
+            {
+                i++;
+                Request_L.uri = $"https://{cfg.Remote_Host}/notes/{obj.Value<string>("id")}";
+                Request_L.i = cfg.Local_Token;
+                Respone = Local.Post("ap/show", System.Text.Json.JsonSerializer.Serialize(Request_L));
+                Console.WriteLine("id:"+obj.Value<string>("id")+$" / Count:{i}\r\n");
+                untilID = obj.Value<string>("id");
+                Task.Delay(cfg.Delay).Wait();
+            }
+            /**/
+            if (untilID == "") { continue; }
+            while (true)
+            {
+                var Request_R2 = new MisskeyRemoteDeliver.NotesRequest2_Rootobject();
+                Request_R2.limit = 100;
+                Request_R2.userId = id;
+                Request_R2.untilId = untilID;
+                 Respone = Remote.Post("users/notes", System.Text.Json.JsonSerializer.Serialize(Request_R2));
+                if(Respone ==null) { break; }
+                if(Respone == "[]") { break; }
+                Result = JsonConvert.DeserializeObject<dynamic>(Respone);
+                foreach (var obj in (Result as Newtonsoft.Json.Linq.JArray))
+                {
+                    i++;
+                    Request_L.uri = $"https://{cfg.Remote_Host}/notes/{obj.Value<string>("id")}";
+                    Request_L.i = cfg.Local_Token;
+                     Respone = Local.Post("ap/show", System.Text.Json.JsonSerializer.Serialize(Request_L));
+                    Console.WriteLine("id:" + obj.Value<string>("id") + $" / Count:{i}\r\n");
+                    untilID = obj.Value<string>("id");
+                    Task.Delay(cfg.Delay).Wait();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            continue;
+        }
+            
+        }
+        Console.WriteLine("エンターで終了");
+        Console.ReadLine();
+    }
+    static AppConfig LoadConfig()
+    {
+       var config = new AppConfig();
+        var Cfg = new ConfigurationBuilder()
+    .AddIniFile(".\\config.ini")
+    .Build();
+        var sec_Remote = Cfg.GetSection("Remote");
+        config.Remote_Token = sec_Remote["Token"];
+        config.Remote_Host = sec_Remote["Host"];
+        var Cnt = Convert.ToInt32(sec_Remote["UserCount"]);
+        for (var i = 0; i < Cnt; i++)
+        {
+            var sec_RemoteUser = Cfg.GetSection($"Remote_User_{i + 1}");
+            config.UserIDs.Add(sec_RemoteUser["id"]);
+        }
+        var sec_Local = Cfg.GetSection("Local");
+        config.Local_Token = sec_Local["Token"];
+        config.Local_Host = sec_Local["Host"];
+
+        var sec_App = Cfg.GetSection("App");
+        config.Delay = Convert.ToInt32(sec_App["Delay"]);
+        return config;
+    
+}
+public class AppConfig
+{
+    public string Remote_Token { get; set; }
+    public string Remote_Host { get; set; }
+    public string Local_Token { get; set; }
+    public string Local_Host { get; set; }
+    public List<string> UserIDs { get; set; } = new List<string>();
+    public int Delay { get; set; }
+}
