5e52693b6c7a3144e88f4f99a3716a22333d763f
pen <pen@penginn.net>
2024-01-05 04:41:30 +0900

    最後にループ回数を表示するようにしてそれ用にカウンタ追加

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using System.Text.Json;
+    
 {
    
         var cfg = LoadConfig();
@@ -9,13 +10,15 @@ using System.Text.Json;
         Remote.Setting(cfg.Remote_Token, cfg.Remote_Host);
         Local.Setting(cfg.Local_Token, cfg.Local_Host);
 
-        foreach (var id in cfg.UserIDs)
-        {
+        var Counts = new List<Counts>();
+    foreach (var id in cfg.UserIDs)
+    {
+        var Count = new Counts() { UserId = id, Count = 0 };
         var i = 0;
         var Request_R1 = new MisskeyRemoteDeliver.NotesRequest_Rootobject();
-            Request_R1.limit = 100;
-            Request_R1.userId = id;
-            var Respone = Remote.Post("users/notes",System.Text.Json.JsonSerializer.Serialize(Request_R1));
+        Request_R1.limit = 100;
+        Request_R1.userId = id;
+        var Respone = Remote.Post("users/notes", System.Text.Json.JsonSerializer.Serialize(Request_R1));
         try
         {
             var Result = JsonConvert.DeserializeObject<dynamic>(Respone);
@@ -29,41 +32,53 @@ using System.Text.Json;
                 Request_L.uri = $"https://{cfg.Remote_Host}/notes/{obj.Value<string>("id")}";
                 Request_L.i = cfg.Local_Token;
                 Respone = Local.Post("ap/show", System.Text.Json.JsonSerializer.Serialize(Request_L));
-                Console.WriteLine("id:"+obj.Value<string>("id")+$" / Count:{i}\r\n");
+                Console.WriteLine("NoteId:" + obj.Value<string>("id") + $" / Count:{i}\r\n");
                 untilID = obj.Value<string>("id");
                 Task.Delay(cfg.Delay).Wait();
             }
             /**/
-            if (untilID == "") { continue; }
+            if (untilID == "")
+            { continue; }
             while (true)
             {
                 var Request_R2 = new MisskeyRemoteDeliver.NotesRequest2_Rootobject();
                 Request_R2.limit = 100;
                 Request_R2.userId = id;
                 Request_R2.untilId = untilID;
-                 Respone = Remote.Post("users/notes", System.Text.Json.JsonSerializer.Serialize(Request_R2));
-                if(Respone ==null) { break; }
-                if(Respone == "[]") { break; }
+                Respone = Remote.Post("users/notes", System.Text.Json.JsonSerializer.Serialize(Request_R2));
+                if (Respone == null) { break; }
+                if (Respone == "[]") { break; }
                 Result = JsonConvert.DeserializeObject<dynamic>(Respone);
                 foreach (var obj in (Result as Newtonsoft.Json.Linq.JArray))
                 {
                     i++;
                     Request_L.uri = $"https://{cfg.Remote_Host}/notes/{obj.Value<string>("id")}";
                     Request_L.i = cfg.Local_Token;
-                     Respone = Local.Post("ap/show", System.Text.Json.JsonSerializer.Serialize(Request_L));
-                    Console.WriteLine("id:" + obj.Value<string>("id") + $" / Count:{i}\r\n");
+                    Respone = Local.Post("ap/show", System.Text.Json.JsonSerializer.Serialize(Request_L));
+                    Console.WriteLine("NoteId:" + obj.Value<string>("id") + $" / Count:{i}\r\n");
                     untilID = obj.Value<string>("id");
                     Task.Delay(cfg.Delay).Wait();
                 }
             }
+
+            Count.Count = i;
+            Counts.Add(Count);
         }
         catch (Exception e)
         {
+            Count.Count = i;
+            Counts.Add(Count);
             Console.WriteLine(e.Message);
             continue;
         }
-            
-        }
+
+    }
+    
+    foreach(var c in Counts)
+    {
+            Console.WriteLine($"RemoteUserId:{c.UserId} / Count:{c.Count}");
+    }
+        
         Console.WriteLine("エンターで終了");
         Console.ReadLine();
     }
@@ -99,4 +114,9 @@ public class AppConfig
     public string Local_Host { get; set; }
     public List<string> UserIDs { get; set; } = new List<string>();
     public int Delay { get; set; }
+}
+public class Counts
+{
+    public string UserId { get; set; }
+    public int Count { get; set; }
 }
